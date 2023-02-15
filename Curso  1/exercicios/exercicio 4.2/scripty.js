0e80a6096ce6b68efc82318fdb3415c0824ead1c
Heliatan-Humberto <heliatan3@gmail.com>
2023-02-15 15:31:55 -0300

    não creio

---
+++ Curso  1/exercicios/exercicio 4.2/scripty.js
@@ -0,0 +1,27 @@
+function calcular() {
+    var num = Number(document.querySelector('input#inumero').value)
+    var res = document.querySelector('select#itabuada')
+    var show  = document.querySelector('div#res')
+
+    if(num < 0) {
+        window.alert('Número invalido!')
+    }
+    else {
+        show.style.display = `grid`
+        let n  = 1
+        res.innerHTML =  ``
+       while (n <= 10) {
+            let item = document.createElement('option')
+            item.text = (`${num} x ${n} = ${num * n }`)
+            res.appendChild(item)
+            n++
+       }
+        
+    }
+}
+function limpar () {
+    res.style.display = `none`
+}
+
+
+

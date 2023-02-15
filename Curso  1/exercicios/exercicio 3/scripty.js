b76f4e81520b5d90ec3b0954650ad6ac84a82723
Heliatan-Humberto <heliatan3@gmail.com>
2023-02-15 10:01:41 -0300

    fix erro script

--- Curso  1/exercicios/exercicio 3/scripty.js
+++ Curso  1/exercicios/exercicio 3/scripty.js
@@ -1,17 +1,25 @@
 function  calcular() {
-    var start = Number(document.querySelector('input#iinicio').value)
-    var end = Number(document.querySelector('input#ifim').value)
-    var res = document.querySelector('div#ires')
-    var conta  = start + 1
-
-    if (start < end) {
-        start++
-
-        
-        res.innerHTML = `${start}`
-    } else {
-        res.innerHTML = `fim`
-    }
-
+    let start = Number(document.querySelector('input#iinicio').value)
+    let end = Number(document.querySelector('input#ifim').value)
+    let passo = Number(document.querySelector('input#ipasso').value)
+    let res = document.querySelector('div#ires')
     
+    if (passo == 0) {
+        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Contagem...'
+    } else {
+        res.innerHTML = `contando: `
+        if(start < end) {
+            for(var c = start; c <= end; c += passo) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            }
+        else {
+            for(var c = start; c >= end; c -= passo) { 
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    } 
 }
+ 
